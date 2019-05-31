7f680f4c256b99e11bd808ed5f93dc20334cfc39
shraddhazore <shraddhak1995@gmail.com>
2019-05-31 13:36:10 +0530

    J0001 : First Commit Print Program pattern

---
+++ montronapp/src/com/montron/patterndisplayer/launcher/Launcher.java
@@ -0,0 +1,20 @@
+package com.montron.patterndisplayer.launcher;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("#####################Part One#######################");
+		String a[]= new String("_*_").split("");
+		for(String string : a){
+			System.out.println(string);
+			}
+		
+		System.out.println("#####################Part Two#######################");
+		String b[]= new String("*_*").split("");
+		for(String string : b){
+			System.out.println(string);
+			}
+		}
+
+}
